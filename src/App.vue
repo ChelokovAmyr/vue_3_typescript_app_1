2bc644d5e5030323b7e1e536b9acb00805da540c
Amyr <44586350+ChelokovAmyr@users.noreply.github.com>
2022-05-12 11:10:38 +0300

    main comit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div class="nav-wrapper position-relative">
+      <ul class="nav nav-pills nav-fill flex-column flex-md-row">
+        <li class="nav-item">
+          <router-link
+              class="nav-link mb-sm-3 mb-md-0"
+              to="/"
+              active-class="active"
+              exact
+          >List Of Notes</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link
+              class="nav-link mb-sm-3 mb-md-0"
+              to="/note"
+              active-class="active"
+              exact
+          >Create Note</router-link
+          >
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <router-view />
+  </div>
 </template>
+
+<script>
+  export default{
+    name: 'App'
+  }
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 5px 10px;
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
 }
-
-nav {
-  padding: 30px;
+@media screen and (max-width: 768px) {
+  #app {
+    padding: 0px 5px;
+  }
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.router-link-exact-active {
+  cursor: auto !important;
 }
 </style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,80 @@
+<template>
+  <teleport to="body">
+    <transition name="fade">
+      <div v-if="showModal" class="modal-backup" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-header">
+            <h2 class="h6 modal-title md-0">
+              <slot name="modal-header">default Title</slot>
+            </h2>
+            <button type="button" class="close" @click="$emit('onClose')">
+              <span aria-hidden="true">x</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>
+              <slot name="modal-body">
+                default text
+              </slot>
+            </p>
+          </div>
+          <div class="modal-footer">
+            <slot name="modal-actions">
+              <p>No Actions</p>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </teleport>
+</template>
+
+<script lang="ts">
+  import {defineComponent, ref, watch} from "vue";
+   const props = {
+     show: {
+       type: Boolean,
+       default: false
+     }
+   }
+
+   const emits = ['onClose']
+   const components = {}
+   export default defineComponent({
+     name:'ModalDialog',
+     props,
+     emits,
+     components,
+     setup(props){
+       const showModal = ref(false)
+
+       watch(
+           ()=> props.show,
+           (show)=> {
+             showModal.value = show
+           }
+       )
+
+       return {
+         showModal
+       }
+     }
+   })
+</script>
+
+<style>
+  .modal-backup{
+    overflow: hidden;
+    outline: 0;
+    background-color: #e6e7eec0 !important;
+  }
+  .fade-enter-active,
+  .fade-leave-active{
+    transition: opacity 0.5s ease;
+  }
+
+  .fade-enter-form,
+  .fade-leave-to{
+    opacity: 0
+  }
+</style>
